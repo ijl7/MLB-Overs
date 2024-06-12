7b9aaf9383e0959fa245d909f81e868089e007b1
ijl7 <62620407+ijl7@users.noreply.github.com>
2024-06-11 20:55:27 -0400

    modified 2 delayed games and wrote score diff

--- overStats.py
+++ overStats.py
@@ -1,7 +1,46 @@
 import csv
 import math
 
-homeScore = 0
-awayScore = 0
-def getScoreDiffIn8th():
-    return
+
+def getScoreDiffMid8th():
+    homeScore = 0
+    awayScore = 0
+    i = 2020
+    while i < 2021:
+        with open('gl' + str(i) + '.csv', mode='r') as file:
+            csvFile = csv.DictReader(file)
+            game = 0
+            for row in csvFile:
+                game += 1
+                awayInning = 0
+                while awayInning < len(row['awayinnings'])-1:
+                    awayinningRuns = row['awayinnings'][awayInning]
+                    if awayinningRuns != '(':
+                        awayScore += int(awayinningRuns)
+                        awayInning += 1
+                    else:
+                        awayScore += int(row['awayinnings'][awayInning+1:awayInning+3])
+                        awayInning += 4
+                homeInning = 0
+                while homeInning < len(row['homeinnings'])-2:
+                    homeInningRuns = row['homeinnings'][homeInning]
+                    if homeInningRuns != '(' and homeInningRuns != 'x':
+                        homeScore += int(homeInningRuns)
+                        homeInning += 1
+                    else:
+                        if homeInningRuns == '(':
+                            homeScore += int(row['homeinnings'][homeInning+1:homeInning+3])
+                            homeInning += 4
+                        else:
+                            homeScore += 0
+                            homeInning += 1
+                print(awayScore-homeScore)
+                awayScore = 0
+                homeScore = 0
+                
+        i += 1
+                #homeScore8th = row[]
+                #return (game, diff)
+
+
+getScoreDiffMid8th()
