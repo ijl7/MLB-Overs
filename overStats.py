76882384a9d81688d955318f4de8c6805dc8d2a4
ijl7 <62620407+ijl7@users.noreply.github.com>
2024-06-11 22:35:38 -0400

    data purifying and added more sound logic

--- overStats.py
+++ overStats.py
@@ -1,6 +1,38 @@
 import csv
 import math
 
+gameDiff = []
+runsAfterTie = []
+
+#going to be used to clean up the code, unused right now
+def getAnyScore(row, awayMax, homeMax):
+    homeScore = 0
+    awayScore = 0
+
+    awayInning = 0
+    while awayInning < awayMax:
+        awayinningRuns = row['awayinnings'][awayInning]
+        if awayinningRuns != '(':
+            awayScore += int(awayinningRuns)
+            awayInning += 1
+        else:
+            awayScore += int(row['awayinnings'][awayInning+1:awayInning+3])
+            awayInning += 4
+    homeInning = 0
+    while homeInning < homeMax:
+        homeInningRuns = row['homeinnings'][homeInning]
+        if homeInningRuns != '(' and homeInningRuns != 'x':
+            homeScore += int(homeInningRuns)
+            homeInning += 1
+        else:
+            if homeInningRuns == '(':
+                homeScore += int(row['homeinnings'][homeInning+1:homeInning+3])
+                homeInning += 4
+            else:
+                homeScore += 0
+                homeInning += 1
+
+    return homeScore + awayScore
 
 def getScoreDiffMid8th():
     homeScore = 0
@@ -13,16 +45,19 @@ def getScoreDiffMid8th():
             for row in csvFile:
                 game += 1
                 awayInning = 0
-                while awayInning < len(row['awayinnings'])-1:
+                awayMax = 8
+                homeMax = 7
+                while awayInning < int(min(awayMax, len(row['awayinnings'][awayInning]))):
                     awayinningRuns = row['awayinnings'][awayInning]
                     if awayinningRuns != '(':
                         awayScore += int(awayinningRuns)
                         awayInning += 1
                     else:
                         awayScore += int(row['awayinnings'][awayInning+1:awayInning+3])
+                        awayMax += 2
                         awayInning += 4
                 homeInning = 0
-                while homeInning < len(row['homeinnings'])-2:
+                while homeInning < int(min(homeMax, len(row['homeinnings'])-1)):
                     homeInningRuns = row['homeinnings'][homeInning]
                     if homeInningRuns != '(' and homeInningRuns != 'x':
                         homeScore += int(homeInningRuns)
@@ -30,17 +65,64 @@ def getScoreDiffMid8th():
                     else:
                         if homeInningRuns == '(':
                             homeScore += int(row['homeinnings'][homeInning+1:homeInning+3])
+                            homeMax += 2
                             homeInning += 4
                         else:
                             homeScore += 0
                             homeInning += 1
-                print(awayScore-homeScore)
+                gameDiff.append((game,homeScore+awayScore,homeScore-awayScore))
                 awayScore = 0
                 homeScore = 0
-                
-        i += 1
-                #homeScore8th = row[]
-                #return (game, diff)
 
+        i += 1
+        
+def getRunsAfterTie():
+    i = 2020
+    while i < 2021:
+        with open('gl' + str(i) + '.csv', mode='r') as file:
+            csvFile = csv.DictReader(file)
+            game = 0
+            for row in csvFile:
+                totalRuns = 0
+                awayScore = 0
+                homeScore = 0
+                game += 1
+                if gameDiff[game-1][2] == 0:
+                    awayInning = 0
+                    while awayInning < len(row['awayinnings']):
+                        awayinningRuns = row['awayinnings'][awayInning]
+                        if awayinningRuns != '(':
+                            awayScore += int(awayinningRuns)
+                            awayInning += 1
+                        else:
+                            awayScore += int(row['awayinnings'][awayInning+1:awayInning+3])
+                            awayInning += 4
+                    homeInning = 0
+                    while homeInning < len(row['homeinnings']):
+                        homeInningRuns = row['homeinnings'][homeInning]
+                        if homeInningRuns != '(' and homeInningRuns != 'x':
+                            homeScore += int(homeInningRuns)
+                            homeInning += 1
+                        else:
+                            if homeInningRuns == '(':
+                                homeScore += int(row['homeinnings'][homeInning+1:homeInning+3])
+                                homeInning += 4
+                            else:
+                                homeScore += 0
+                                homeInning += 1
+                    totalRuns = homeScore + awayScore
+                    runsAfterTie.append(totalRuns - gameDiff[game-1][1])
+
+        i += 1
 
 getScoreDiffMid8th()
+getRunsAfterTie()
+over = 0
+under = 0
+for runs in runsAfterTie:
+    if runs > 1:
+        over += 1
+    else:
+        under += 1
+print('\nIn 2020 there was an average of ' + str(round(sum(runsAfterTie)/len(runsAfterTie), 3)) + ' runs scored in games tied in the mid 8th.\n')
+print(str(over) + ' of ' + str(len(runsAfterTie)) + ' games went over, or ' + str(round((over/len(runsAfterTie))*100, 3)) + '%.')
